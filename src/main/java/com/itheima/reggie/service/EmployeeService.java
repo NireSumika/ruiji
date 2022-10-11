a098d024af71168233b3a47447bb2a2e94b0bfd4
NireSumika <zt2442192854@yahoo.com>
2022-10-11 23:18:54 +0800

    structs

---
+++ src/main/java/com/itheima/reggie/service/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.itheima.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.itheima.reggie.entity.Employee;
+/**
+ * @author Sora
+ * @Description
+ * @Auther Terence Zhang
+ * @Datetime 22/10/11 22:54
+ **/
+public interface EmployeeService extends IService<Employee> {
+}
