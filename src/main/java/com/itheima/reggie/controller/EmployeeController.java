a282b8badab0f99d50c8c458c282621c59698b49
68341 <2442192854@qq.com>
2022-10-12 16:34:24 +0800

    login

--- src/main/java/com/itheima/reggie/controller/EmployeeController.java
+++ src/main/java/com/itheima/reggie/controller/EmployeeController.java
@@ -1,10 +1,18 @@
 package com.itheima.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.entity.Employee;
 import com.itheima.reggie.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Sora
@@ -19,4 +27,42 @@ public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
+
+    /* 员工登录 */
+    @PostMapping("/login")
+    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee){
+        /*
+        * 1、将页面提交的密码加密
+        * 2、根据用户名查数据库
+        * 3、比对密码
+        * 4、查询员工状态
+        * 5、成功，将员工id存入Session并返回登录成功结果
+          */
+        //1
+        String password = employee.getPassword();
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
+
+        //2
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Employee::getUsername, employee.getUsername());
+        Employee emp = employeeService.getOne(queryWrapper);
+        if(null == emp){
+            return R.error("用户不存在");
+        }
+
+        //3
+        if(!emp.getPassword().equals(password)){
+            return R.error("密码错误");
+        }
+
+        //4
+        if(0 == emp.getStatus()){
+            return R.error("账号已禁用");
+        }
+
+        //5
+        request.getSession().setAttribute("employee", emp.getId());
+
+        return R.success(emp);
+    }
 }
