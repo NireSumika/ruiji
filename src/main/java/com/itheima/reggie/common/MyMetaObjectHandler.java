641c23ec53daad06fc179723f7b5763916beeddd
NireSumika <zt2442192854@yahoo.com>
2022-10-17 23:01:48 +0800

    公共字段自动填充-1

---
+++ src/main/java/com/itheima/reggie/common/MyMetaObjectHandler.java
@@ -0,0 +1,46 @@
+package com.itheima.reggie.common;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * @Description
+ * @Auther Terence Zhang
+ * @Datetime 22/10/17 22:41
+ **/
+@Component
+@Slf4j
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    /**
+     * @description 插入操作，自动填充
+     * @param metaObject
+     */
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.info("公共字段自动填充[insert]");
+        log.info(metaObject.toString());
+        metaObject.setValue("creatTime", LocalDateTime.now());
+        metaObject.setValue("updateTime", LocalDateTime.now());
+        //TODO 获取id
+        metaObject.setValue("creatUser", new Long(1));
+        metaObject.setValue("updateUser", new Long(1));
+
+    }
+
+    /**
+     * @description 更新操作，自动填充
+     * @param metaObject
+     */
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.info("公共字段自动填充[update]");
+        log.info(metaObject.toString());
+        //TODO 获取id
+        metaObject.setValue("updateTime", LocalDateTime.now());
+        metaObject.setValue("updateUser", new Long(1));
+    }
+}
