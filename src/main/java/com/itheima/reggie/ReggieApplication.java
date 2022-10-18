41ac1c2379ac3d80c2f1699aeca84e8a690a0580
68341 <2442192854@qq.com>
2022-10-18 18:03:11 +0800

    other

--- src/main/java/com/itheima/reggie/ReggieApplication.java
+++ src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -12,6 +12,6 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 public class ReggieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class, args);
-        log.info("success!");
+        log.info("success startup!");
     }
 }
