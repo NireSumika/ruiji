00f90a4d10068ca76811c74ce8747411ecf457cd
NireSumika <zt2442192854@yahoo.com>
2022-10-19 23:10:36 +0800

    分类

--- src/main/java/com/itheima/reggie/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/itheima/reggie/service/impl/CategoryServiceImpl.java
@@ -1,5 +1,6 @@
 package com.itheima.reggie.service.impl;
 
+
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.itheima.reggie.entity.Category;
 import com.itheima.reggie.mapper.CategoryMapper;
@@ -12,5 +13,5 @@ import org.springframework.stereotype.Service;
  * @Datetime 22/10/19 22:57
  **/
 @Service
-public class CategoryServiceImpl extends ServiceImpl<CategoryMapper,Category> implements CategoryService {
+public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> implements CategoryService {
 }
