867eba66d17d7d1cf756d0c5c10f921cbb227ce4
68341 <2442192854@qq.com>
2022-10-21 16:33:27 +0800

    upload

--- src/main/java/com/itheima/reggie/controller/CommonController.java
+++ src/main/java/com/itheima/reggie/controller/CommonController.java
@@ -1,5 +1,6 @@
-package com.itheima.reggie.common;
+package com.itheima.reggie.controller;
 
+import com.itheima.reggie.common.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
